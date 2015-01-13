cb93232184140bbf62a1b4e562348ab26d485f49
Alex <sqqrew@gmail.com>
2015-01-13 18:19:31 +0300

    .

---
+++ app/src/main/java/com/a_zona/slidingmenu/fragment/PhotosFragment.java
@@ -0,0 +1,31 @@
+package com.a_zona.slidingmenu.fragment;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.a_zona.slidingmenu.R;
+
+/**
+ * Created by Alexey Filyushin (sqqrew@gmail.com) on 13.01.2015.
+ * Package: com.a_zona.slidingmenu.fragment
+ * Project: SlidingMenu
+ * <p/>
+ * Copyright (c) 2014 A-Zona, Inc.
+ */
+public class PhotosFragment extends Fragment {
+
+    public PhotosFragment(){}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
+
+        return rootView;
+    }
+}
